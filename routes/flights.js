13d0442577cd032f85705f1eba6ec04d04928062
Alanna Celentano <alannacelentano@Alannas-MacBook-Pro.local>
2020-02-24 16:34:39 -0600

    add destinations

--- routes/flights.js
+++ routes/flights.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 const flightsCtrl = require("../controllers/flights");
 
-/* GET users listing. */
 router.get("/", flightsCtrl.index);
 router.get("/new", flightsCtrl.new);
 router.get("/:id", flightsCtrl.show);
